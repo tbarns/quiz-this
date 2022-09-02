3b6212702a10e8492c598409a14ff7e0b246f8eb
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-02 16:54:23 -0500

    updated stylesheet, added a timer and start button to html and JS

---
+++ script.js
@@ -0,0 +1,26 @@
+var timerEl = document.querySelector(".timer");
+var start = document.querySelector(".start")
+
+var timeRemaining = 5;
+
+var start = document.getElementById("start");
+
+start.addEventListener("click", startTime);
+
+
+
+function startTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      timeRemaining--;
+      timerEl.textContent = timeRemaining + " seconds remaining!";
+  
+      if(timeRemaining === 0) {
+         clearInterval(timerInterval);
+        //window.alert("Time's up!");
+//             return;
+      }
+  
+    }, 1000);
+  }
+start.setAttribute("style", "font-size: 40px; background-color: pink; border: 10px solid green; border-radius: 15px")
