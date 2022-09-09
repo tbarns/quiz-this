de258e38449e1faa7a0783fcb7aadaecc6081fee
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-08 20:29:51 -0500

    added text to questions so that they have real answers and incorrect responses

--- script.js
+++ script.js
@@ -6,70 +6,58 @@ var currentquestion= 0;
 var score = 0;
 var box = document.querySelector("#quizBox")
 
-var questions = [{
-  questionTitle: "Who is Rupaul?",
-  choices: ["Mother", "The Queen of Drag", "My inner saboteur", "All of the above"],
-  answer: "All of the above"
-},
-{
-  questionTitle: "ADD A QUESTION2",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION3",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION4",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION5",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION6",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION7",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-}];
+// var questions = [{
+//   questionTitle: "Who is Rupaul?",
+//   choices: ["Mother", "The Queen of Drag", "My inner saboteur", "All of the above"],
+//   answer: "All of the above"
+// },
+// {
+//   questionTitle: "Who should have been Blac Chyna?",
+//   choices: ["Nina Bo Nina Brown", "Valentina", "Charro", "Farrah Moan"],
+//   answer: "Farrah Moan"
+// },
+// {
+//   questionTitle: "Which country does NOT have a Drag Race franchise?",
+//   choices: ["Canada", "Thailand", "Holland", "Belarus"],
+//   answer: "4"
+// },
+// {
+//   questionTitle: "UK Hun is the name of a __________.",
+//   choices: ["style of drag", "chart topping song from the drag race", "traditional greeting of Liverpool", "slag for a wig"],
+//   answer: "2"
+// },
+// {
+//   questionTitle: "Who is Michelle Visage?",
+//   choices: ["a type of chair", "a New Jersey born circus performer", "Rupaul's best friend", "a fashion model"],
+//   answer: "3"
+// },
+// {
+//   questionTitle: "ADD A QUESTION6",
+//   choices: ["1", "2", "3", "4"],
+//   answer: "4"
+// },
+// {
+//   questionTitle: "ADD A QUESTION7",
+//   choices: ["1", "2", "3", "4"],
+//   answer: "4"
+// }];
 
-var pEl = document.createElement('p')
-var button = document.createElement("button")
-pEl.textContent = questions[0].questionTitle
+// var pEl = document.createElement('p')
+// var button = document.createElement("button")
+// pEl.textContent = questions[0].questionTitle
 
-box.appendChild(pEl)
-
-
-questions.choices.forEach(function(button, choice){
-  var button = document.createElement("button")
-  button.textContent= choice
-  box.appendChild(button)
-})
-button.addEventListener("click", localStorage.setItem("test", JSON.stringify(questions.choices[0])))
+// box.appendChild(pEl)
 
 
-// // FUNCTION that places the questionHolder information inot the proper section
-// 	function setQuestionData () {
-// 		queryElement('#quizBox p').innerHTML = questions[currentquestion].title;
-// 		queryElement('#quizBox button:nth-of-type(1)').innerHTML = `1. ${questions[currentquestion].choices[0]}`;
-// 		queryElement('#quizBox button:nth-of-type(2)').innerHTML = `2. ${questions[currentquestion].choices[1]}`;
-// 		queryElement('#quizBox button:nth-of-type(3)').innerHTML = `3. ${questions[currentquestion].choices[2]}`;
-// 		queryElement('#quizBox button:nth-of-type(4)').innerHTML = `4. ${questions[currentquestion].choices[3]}`;
-//     queryElement('#quizBox button:nth-of-type(5)').innerHTML = `5. ${questions[currentquestion].choices[4]}`;
-//     queryElement('#quizBox button:nth-of-type(6)').innerHTML = `6. ${questions[currentquestion].choices[5]}`;
-//     queryElement('#quizBox button:nth-of-type(7)').innerHTML = `7. ${questions[currentquestion].choices[6]}`;
+// questions.choices.forEach(function(button, choice){
+//   var button = document.createElement("button")
+//   button.textContent= choice
+//   box.appendChild(button)
+// })
+// button.addEventListener("click", localStorage.setItem("test", JSON.stringify(questions.choices[0])))
 
-//   }
-var start = document.getElementById("start");
+
+
 
 start.addEventListener("click", startTime);
 
