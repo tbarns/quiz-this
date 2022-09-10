f9b7854b3179304a1e0d6aef723913ef907e148d
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-09 20:35:52 -0500

    added buttons in HTML with data-number to allow for the JS to push the values inot them

--- script.js
+++ script.js
@@ -2,57 +2,46 @@ var timerEl = document.querySelector(".timer");
 var start = document.querySelector("#start")
 var timeRemaining = 5;
 var index = 0;
-var currentquestion= 0;
+var currentquestion= {};
 var score = 0;
-var box = document.querySelector("#quizBox")
+var box = document.querySelector(".quizBox")
 
 var questions = [{
   questionTitle: "Who is Rupaul?",
   choices: ["Mother", "The Queen of Drag", "My inner saboteur", "All of the above"],
-  answer: "All of the above"
+  answers: "All of the above"
 },
 {
   questionTitle: "Who should have been Blac Chyna?",
   choices: ["Nina Bo Nina Brown", "Valentina", "Charro", "Farrah Moan"],
-  answer: "Farrah Moan"
+  answers: "Farrah Moan"
 },
 {
   questionTitle: "Which country does NOT have a Drag Race franchise?",
   choices: ["Canada", "Thailand", "Holland", "Belarus"],
-  answer: "4"
+  answers: "4"
 },
 {
   questionTitle: "UK Hun is the name of a __________.",
   choices: ["style of drag", "chart topping song from the drag race", "traditional greeting of Liverpool", "slag for a wig"],
-  answer: "2"
+  answers: "2"
 },
 {
   questionTitle: "Who is Michelle Visage?",
   choices: ["a type of chair", "a New Jersey born circus performer", "Rupaul's best friend", "a fashion model"],
-  answer: "3"
+  answers: "3"
 },
-{
-  questionTitle: "ADD A QUESTION6",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-},
-{
-  questionTitle: "ADD A QUESTION7",
-  choices: ["1", "2", "3", "4"],
-  answer: "4"
-}];
+];
 
-var pEl = document.createElement('p')
-var button = document.createElement("button")
-pEl.textContent = questions[0].questionTitle
-
-box.appendChild(pEl)
+var qEl = document.createElement('h2')
+var choices = Array.from(document.querySelectorAll(".answers"))
+qEl.textContent = questions[0].questionTitle
 
 
 for (var i=0; i < questions.length; i++){
     var currentquestion = questions[i].questionTitle
     var userAnswer = questions[i].choices
-    if (userAnswer == "questions[i].answer"){
+    if (userAnswer == "questions[i].answers"){
     score++;
     }
     if (i > questions.length) {
@@ -63,14 +52,14 @@ for (var i=0; i < questions.length; i++){
   }
   
   
-  
+  //localStorage.setItem
 
 
 // questions.choices.forEach(function(choice){
 //   var btns = document.createElement("#quizBox")
 //   btns.textContent= choice
 //   box.appendChild(choice)
-// // })
+// })
 // box.addEventListener("click", localStorage.setItem("test", JSON.stringify(questions.choices[0])))
 
 
